a99c4ea835df80cfb4fd1982a84ca280b1df14a2
Waswa Olunga <32232146+iamwaswa@users.noreply.github.com>
2022-01-08 12:36:08 -0800

    refactor: update RenderList component

--- components/RenderList.component.tsx
+++ components/RenderList.component.tsx
@@ -1,29 +1,32 @@
-type RenderListProps<TData> = {
-  items: Array<TData>;
-  renderNoContent?: () => JSX.Element;
-  renderItem(item: TData, index: number, items: Array<TData>): JSX.Element;
+import { useCallback } from 'react';
+
+type RenderListProps<DataType> = {
+  items: Array<DataType>;
+  renderNoContent?: JSX.Element | (() => JSX.Element);
+  renderItem(
+    item: DataType,
+    index: number,
+    items: Array<DataType>
+  ): JSX.Element;
 };
 
-export function RenderList<TData>({
+export function RenderList<DataType>({
   items,
-  renderNoContent,
+  renderNoContent = <></>,
   renderItem,
-}: RenderListProps<TData>): JSX.Element {
-  function renderListItem(
-    item: TData,
-    index: number,
-    array: Array<TData>
-  ): JSX.Element | null {
-    return renderItem(item, index, array);
-  }
-
-  if (items.length === 0 && renderNoContent) {
-    return renderNoContent();
-  }
+}: RenderListProps<DataType>): JSX.Element | Array<JSX.Element> {
+  const renderListItem = useCallback(
+    (item: DataType, index: number, array: Array<DataType>): JSX.Element => {
+      return renderItem(item, index, array);
+    },
+    [renderItem]
+  );
 
   if (items.length === 0) {
-    return <></>;
+    return typeof renderNoContent === `function`
+      ? renderNoContent()
+      : renderNoContent;
   }
 
-  return <>{items.map(renderListItem)}</>;
+  return items.map(renderListItem);
 }
