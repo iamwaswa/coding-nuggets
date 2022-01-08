30b5e2f788af1474f84d90f3f6843e931ee064f7
Waswa Olunga <32232146+iamwaswa@users.noreply.github.com>
2022-01-08 13:53:26 -0800

    refactor: rename classes to classNames

--- utils/MergeClassNames.utils.ts
+++ utils/MergeClassNames.utils.ts
@@ -1,9 +1,9 @@
 /**
- * Merges an array of classes.
- * @param classNames An array of classes to merge
- * @returns a string containing all the classes merged
+ * Merges an array of classNames.
+ * @param classNames An array of classNames to merge
+ * @returns a string containing all the classNames merged
  */
-export function mergeClasses(...classNames: Array<unknown>): string {
+export function mergeClassNames(...classNames: Array<unknown>): string {
   return classNames.reduce(
     (mergedClasses: string, className: unknown): string => {
       if (typeof className !== `string`) {
