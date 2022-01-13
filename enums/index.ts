ac5e70cdd562b45c15f4dd1255a4e9c511c3360e
Waswa Olunga <32232146+iamwaswa@users.noreply.github.com>
2022-01-13 03:51:13 -0800

    Update index.ts

--- enums/index.ts
+++ enums/index.ts
@@ -20,13 +20,3 @@ export enum DatabaseErrorCode {
   DOCUMENT_ALREADY_EXISTS = 'Document already exists',
   NON_EXISTENT_DOCUMENT = 'Non existent document',
 }
-
-export enum Direction {
-  LTR = 'ltr',
-  RTL = 'rtl',
-}
-
-export enum ThemeState {
-  DARK = 'dark',
-  LIGHT = 'light',
-}
