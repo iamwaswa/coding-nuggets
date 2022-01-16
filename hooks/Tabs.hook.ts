e238929165d8c64eaca929f7936600510a753c40
Waswa Olunga <32232146+iamwaswa@users.noreply.github.com>
2022-01-16 15:33:54 -0800

    Add default tab to useTabs hook

--- hooks/Tabs.hook.ts
+++ hooks/Tabs.hook.ts
@@ -3,14 +3,16 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 /**
  * Manages the state of tabs.
  * @param tabs The tabs to use
+ * @param defaultTab The default tab to set
  * @returns The tabs state
  */
 export function useTabs<TabType>(
-  tabs: Array<TabType>
+  tabs: Array<TabType>,
+  defaultTab?: TabType
 ): [TabType, Array<TabType>, (tab: TabType) => void] {
   const currentTabs = useRef<string>(JSON.stringify(tabs));
 
-  const [currentTabValue, setCurrentTabValue] = useState<TabType>(tabs[0]);
+  const [currentTabValue, setCurrentTabValue] = useState<TabType>(defaultTab ?? tabs[0]);
 
   // * If the tabs have changed
   // * then reset the current tab value
